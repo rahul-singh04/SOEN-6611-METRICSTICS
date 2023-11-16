778117568d594a4f1c3c7d59d0427f077bc011a4
amrinderpreetsingh <amrinderpreet946@gmail.com>
2023-11-16 10:09:56 -0500

    added unit test cases

--- Code/data_processor_tests.py
+++ Code/data_processor_tests.py
@@ -1,16 +1,56 @@
+import csv
 import unittest
+from data_processor import DataProcessor
 
-from Code.data_processor import DataProcessor
+
+class TestDataProcessor(unittest.TestCase):
+
+    def setUp(self):
+        # Create a DataProcessor instance for testing
+        self.data_processor = DataProcessor()
+
+    def test_generate_random_data(self):
+        # Test generating random data
+        self.data_processor.generate_random_data()
+        self.assertTrue(self.data_processor.original_data)
+        self.assertEqual(len(self.data_processor.original_data), 1000)
+
+    def test_get_mean(self):
+        # Test calculating the mean
+        self.data_processor.original_data = [1, 2, 3, 4, 5]
+        mean = self.data_processor.get_mean()
+        self.assertEqual(mean, 3.0)
+
+    def test_get_median_odd_length(self):
+        # Test calculating the median with odd-length data
+        self.data_processor.original_data = [1, 2, 3, 4, 5]
+        median = self.data_processor.get_median()
+        self.assertEqual(median, 3.0)
+
+    def test_get_median_even_length(self):
+        # Test calculating the median with even-length data
+        self.data_processor.original_data = [1, 2, 3, 4, 5, 6]
+        median = self.data_processor.get_median()
+        self.assertEqual(median, 3.5)
+
+    def test_get_mode(self):
+        # Test calculating the mode
+        self.data_processor.original_data = [1, 2, 2, 3, 4, 4, 4]
+        mode = self.data_processor.get_mode()
+        self.assertEqual(mode, 4)
+
+    def test_get_standard_deviation(self):
+        # Test calculating the standard deviation
+        self.data_processor.original_data = [1, 2, 3, 4, 5]
+        std_deviation = self.data_processor.get_standard_deviation()
+        self.assertAlmostEqual(std_deviation, 1.41, places=2)
+
+    def test_get_mean_absolute_deviation(self):
+        # Test calculating the mean absolute deviation
+        self.data_processor.original_data = [1, 2, 3, 4, 5]
+        mean_abs_dev = self.data_processor.get_mean_absolute_deviation()
+        self.assertEqual(mean_abs_dev, 1.2)
 
 
-class DataProcessorTests(unittest.TestCase):
-    def test_get_median(self):
-        processor = DataProcessor()
-        data = [1, 2, 3, 4, 5]
-        processor.original_data = data
-        self.assertEqual(processor.get_median(), 3)
-
-        processor = DataProcessor()
-        data = [1, 2, 3, 4]
-        processor.original_data = data
-        self.assertEqual(processor.get_median(), 2.5)
+if __name__ == '__main__':
+    unittest.main()
